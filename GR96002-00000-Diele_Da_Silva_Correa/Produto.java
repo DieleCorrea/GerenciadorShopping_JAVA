730e57a1fe94d05028718f352a60d0c9ffeec0e6
Diele <dybs1520@gmail.com>
2025-10-30 00:35:11 -0300

    [FEAT] Add new attributes in class Produto.java

--- GR96002-00000-Diele_Da_Silva_Correa/Produto.java
+++ GR96002-00000-Diele_Da_Silva_Correa/Produto.java
@@ -2,21 +2,19 @@ public class Produto {
     private String nome;
     private double preco;
     private Data data;
-
+//CONTRUTOR
     public Produto(String nome, double preco, Data data) {
         this.nome = nome;
         this.preco = preco;
         this.data = data;
     }
-
+//ACESSO E TOSTRING
     public Data getData() {
         return data;
     }
-
     public void setData(Data data) {
         this.data = data;
     }
-
     public String getNome() {
         return nome;
     }
@@ -29,12 +27,16 @@ public class Produto {
     public void setPreco(double preco) {
         this.preco = preco;
     }
-
     @Override
     public String toString() {
         return "Produto{" +
                 "nome='" + nome + '\'' +
                 ", preco=" + preco +
                 '}';
+    }
+ //VALIDAÇÕES
+    public boolean estaVencido(Data dataProduto){
+        if(dataProduto. > Data)
+        return true;
     }
 }
