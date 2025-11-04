90195ef2a76930acd705f7dde7995ad5567a8a4b
Diele <dybs1520@gmail.com>
2025-11-03 23:52:23 -0300

    [FEAT] New class: Vestuario.java

--- GR96002-00000-Diele_Da_Silva_Correa/Vestuario.java
+++ GR96002-00000-Diele_Da_Silva_Correa/Vestuario.java
@@ -1,3 +1,26 @@
-public class Vestuario{
-    
+public class Vestuario extends Loja{
+    private boolean produtosImportados;
+
+    public Vestuario(String nome, int quantidadeFuncionarios, int salarioBaseFuncionario, Endereco endereco, Data dataFundacao, boolean produtosImportados) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+
+    public Vestuario(String nome, int quantidadeFuncionarios, boolean produtosImportados) {
+        super(nome, quantidadeFuncionarios);
+        this.produtosImportados = produtosImportados;
+    }
+
+    public boolean isProdutosImportados() {
+        return produtosImportados;
+    }
+
+    public void setProdutosImportados(boolean produtosImportados) {
+        this.produtosImportados = produtosImportados;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Vestuario{" + "produtosImportados=" + produtosImportados + '}';
+    }
 }
