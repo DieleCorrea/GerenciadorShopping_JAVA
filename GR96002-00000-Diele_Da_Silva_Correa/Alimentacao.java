dd9461e37522b3c5649c394dabc85cf407cbb64c
Diele <dybs1520@gmail.com>
2025-11-03 23:58:45 -0300

    [FEAT] New class: Alimentacao..java

--- GR96002-00000-Diele_Da_Silva_Correa/Alimentacao.java
+++ GR96002-00000-Diele_Da_Silva_Correa/Alimentacao.java
@@ -1,3 +1,26 @@
-public class Alimentacao {
-    
+public class Alimentacao extends Loja{
+    private Data dataAlvara;
+
+    public Alimentacao(String nome, int quantidadeFuncionarios, int salarioBaseFuncionario, Endereco endereco, Data dataFundacao, Data dataAlvara) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.dataAlvara = dataAlvara;
+    }
+
+    public Alimentacao(String nome, int quantidadeFuncionarios, Data dataAlvara) {
+        super(nome, quantidadeFuncionarios);
+        this.dataAlvara = dataAlvara;
+    }
+
+    public Data getDataAlvara() {
+        return dataAlvara;
+    }
+
+    public void setDataAlvara(Data dataAlvara) {
+        this.dataAlvara = dataAlvara;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Alimentacao{" + "dataAlvara=" + dataAlvara + '}';
+    }
 }
