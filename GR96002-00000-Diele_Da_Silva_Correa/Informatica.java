dd6e3c292a590637ca1a79963107f5ff2649b5f3
Diele <dybs1520@gmail.com>
2025-11-04 00:03:39 -0300

    [FEAT] New class: Informatica..java

--- GR96002-00000-Diele_Da_Silva_Correa/Informatica.java
+++ GR96002-00000-Diele_Da_Silva_Correa/Informatica.java
@@ -1,3 +1,28 @@
-public class Informatica  {
-   
+public class Informatica  extends Loja{
+   private double seguroEletronicos;
+
+    public Informatica(String nome, int quantidadeFuncionarios, int salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double seguroEletronicos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Informatica{" + "seguroEletronicos=" + seguroEletronicos + '}';
+    }
+
+    public double getSeguroEletronicos() {
+        return seguroEletronicos;
+    }
+
+    public void setSeguroEletronicos(double seguroEletronicos) {
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    public Informatica(String nome, int quantidadeFuncionarios, double seguroEletronicos) {
+        super(nome, quantidadeFuncionarios);
+        this.seguroEletronicos = seguroEletronicos;
+
+
+    }
 }
