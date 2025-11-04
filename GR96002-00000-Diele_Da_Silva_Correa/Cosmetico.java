5438a5ee6747f350ae38d325651e079006db0693
Diele <dybs1520@gmail.com>
2025-11-03 23:44:54 -0300

    [FEAT] New class Cosmetico

--- GR96002-00000-Diele_Da_Silva_Correa/Cosmetico.java
+++ GR96002-00000-Diele_Da_Silva_Correa/Cosmetico.java
@@ -1,3 +1,26 @@
-public class Cosmetico{
-   
+public class Cosmetico extends Loja{
+    private double taxaComercializacao;
+
+    public Cosmetico(String nome, int quantidadeFuncionarios, int salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double taxaComercializacao) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
+    public Cosmetico(String nome, int quantidadeFuncionarios, double taxaComercializacao) {
+        super(nome, quantidadeFuncionarios);
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
+    public double getTaxaComercializacao() {
+        return taxaComercializacao;
+    }
+
+    public void setTaxaComercializacao(double taxaComercializacao) {
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\ntaxaComercializacao=" + taxaComercializacao;
+    }
 }
